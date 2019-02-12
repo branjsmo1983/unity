4a2de4a0109e555e57f52d8c2736263719da8ffd
Adamo Branz <branjsmo@email.it>
2019-02-12 18:02:24 +0100

    fare_test_pozzetti
    commentata versione che clonava invece di spostare le carte dal mazzo iniziale
    fatto i mazzetti ma sono da testare

--- BurracoTest/Assets/MyAssets/MyScripts/Burraco.cs
+++ BurracoTest/Assets/MyAssets/MyScripts/Burraco.cs
@@ -16,10 +16,18 @@ public class Burraco : MonoBehaviour
 	GameObject deckFake;
 
 	[SerializeField]
+	GameObject playingDeckPosition;
+
+	[SerializeField]
+	GameObject cockpitPosition;
+
+	[SerializeField]
 	internal GameObject[] hands = new GameObject[4];										//per sapere la transform
 
 	[SerializeField]
 	internal GameObject[] cards = new GameObject[4];                                        //per sapere la transform
+
+	internal List<Card> firstCockpit, secondCockpit;
 
 	private List<CardForStartGame> myCardsForStart = new List<CardForStartGame>();
 	private string playerstart;
@@ -88,11 +96,15 @@ public class Burraco : MonoBehaviour
 
 	IEnumerator SelectPlayerStartGame()
 	{
+		List<int> randomNumbers = new List<int>();
 		System.Random random = new System.Random();
 		for(int i = 0; i<4 ; i++)
 		{
+			int index;
 			yield return new WaitForSeconds(0.5f);
-			int index = random.Next(deckForStartGame.deck.Count);
+			do index = random.Next(deckForStartGame.deck.Count);
+			while (randomNumbers.Contains(index));
+			randomNumbers.Add(index);
 			CardForStartGame newCard = Instantiate(deckForStartGame.deck[index], new Vector3(cards[i].transform.position.x, cards[i].transform.position.y, cards[i].transform.position.z), Quaternion.identity, cards[i].transform);
 			newCard.AbsoluteValue = index;
 			newCard.gameObject.tag = cards[i].tag;
@@ -120,81 +132,194 @@ public class Burraco : MonoBehaviour
 		}
 		deckForStartGame.gameObject.SetActive(false);
 		deckFake.SetActive(false);
-		StartCoroutine(CreateMystartHand());
+		//per adesso funziona ma non so come gestire le carte
+		//StartCoroutine(CreateMyStartHand());
+		StartCoroutine(InitialBunches());
 
 	}
 
-
-	IEnumerator CreateMystartHand()
+	IEnumerator InitialBunches()
 	{
-		
 		int newIndex;
 		float xOffset = 0;
 		float zOffset = 0.03f;
 		float yOffset = 0;
-		for (int i = 0; i < 11; i++)  
+		float z = 0;
+		foreach (Card card in deck.myDeck)
 		{
-			
-		
+			card.transform.position = new Vector3(playingDeckPosition.transform.position.x , playingDeckPosition.transform.position.y, playingDeckPosition.transform.position.z - z);
+			card.IsVisible = false;
+			z += 0.02f;
+		}
+		for (int i = 0; i < 11; i++)
+		{
+
+
 			for (int indexPlayer = 0; indexPlayer < 4; indexPlayer++)
 			{
 				newIndex = (indexPlayer + GetOffset()) % 4;
-				
+
 				yield return new WaitForSeconds(0.7f);
-				Card newCard;
-				if(hands[newIndex].tag == "me")
-				{ 
-					newCard = Instantiate(deck.myDeck[0], new Vector3(hands[newIndex].transform.position.x + xOffset, hands[newIndex].transform.position.y, hands[newIndex].transform.position.z - zOffset),Quaternion.identity, hands[newIndex].transform);
+				Card newCard = deck.myDeck[deck.myDeck.Count -1];
+				if (hands[newIndex].tag == "me")
+				{
+					newCard.transform.position = new Vector3(hands[newIndex].transform.position.x + xOffset, hands[newIndex].transform.position.y, hands[newIndex].transform.position.z - zOffset);
+					newCard.transform.rotation = Quaternion.identity;
 					me.myHand.Add(newCard);
-					deck.myDeck.RemoveAt(0);
-					
+					deck.myDeck.RemoveAt(deck.myDeck.Count - 1);
+
 				}
-				else if(hands[newIndex].tag == "myMate")
+				else if (hands[newIndex].tag == "myMate")
 				{
-					newCard = Instantiate(deck.myDeck[0], new Vector3(hands[newIndex].transform.position.x + xOffset, hands[newIndex].transform.position.y, hands[newIndex].transform.position.z - zOffset), Quaternion.identity, hands[newIndex].transform);
+					newCard.transform.position = new Vector3(hands[newIndex].transform.position.x + xOffset, hands[newIndex].transform.position.y, hands[newIndex].transform.position.z - zOffset);
+					newCard.transform.rotation = Quaternion.identity;
 					myMate.myHand.Add(newCard);
-					deck.myDeck.RemoveAt(0);
+					deck.myDeck.RemoveAt(deck.myDeck.Count - 1);
 				}
-				else if(hands[newIndex].tag == "leftOpponent")
+				else if (hands[newIndex].tag == "leftOpponent")
 				{
-					newCard = Instantiate(deck.myDeck[0], new Vector3(hands[newIndex].transform.position.x, hands[newIndex].transform.position.y - yOffset, hands[newIndex].transform.position.z - zOffset), Quaternion.Euler(0, 0, 90), hands[newIndex].transform);
+					newCard.transform.position = new Vector3(hands[newIndex].transform.position.x , hands[newIndex].transform.position.y - yOffset, hands[newIndex].transform.position.z - zOffset);
+					newCard.transform.rotation = Quaternion.Euler(0, 0, 90);
 					leftOpponent.myHand.Add(newCard);
-					deck.myDeck.RemoveAt(0);
+					deck.myDeck.RemoveAt(deck.myDeck.Count - 1);
 				}
 				else
 				{
-					newCard = Instantiate(deck.myDeck[0], new Vector3(hands[newIndex].transform.position.x, hands[newIndex].transform.position.y - yOffset, hands[newIndex].transform.position.z - zOffset), Quaternion.Euler(0, 0, 90), hands[newIndex].transform);
+					newCard.transform.position = new Vector3(hands[newIndex].transform.position.x , hands[newIndex].transform.position.y -yOffset, hands[newIndex].transform.position.z - zOffset);
+					newCard.transform.rotation = Quaternion.Euler(0, 0, 90);
 					rightOpponent.myHand.Add(newCard);
-					deck.myDeck.RemoveAt(0);
+					deck.myDeck.RemoveAt(deck.myDeck.Count - 1);
 				}
 
 				newCard.IsVisible = hands[newIndex].tag == "me" ? true : false;				//vedo solo le mie
+
 			}
 			xOffset += 0.9f;
 			zOffset += 0.03f;
 			yOffset += 0.7f;
 		}
-
-		print("La lista delle mie carte è : ");
-		foreach(Card card in me.myHand)
+		zOffset = 0;
+		for (int i = 0; i < 11; i++)
 		{
-			print(card.Value + " " + card.Suit + " " + card.Color);
-		}
-		me.CountJolly();
-		me.CountPins();
-		myMate.CountJolly();
-		myMate.CountPins();
-		leftOpponent.CountJolly();
-		leftOpponent.CountPins();
-		rightOpponent.CountJolly();
-		rightOpponent.CountJolly();
-		print("Il numero di Jolly che ho io è : " + me.NumberOfJolly + " Il numero di Pinelle è : " + me.NumberOfPins);
-		print("Il numero di Jolly che ha Left è : " + leftOpponent.NumberOfJolly + " Il numero di Pinelle  è : " + leftOpponent.NumberOfPins);
-		print("Il numero di Jolly che ha myMate : " + myMate.NumberOfJolly + " Il numero di Pinelle è : " + myMate.NumberOfPins);
-		print("Il numero di Jolly che ha Right è : " + rightOpponent.NumberOfJolly + " Il numero di Pinelle  è : " + rightOpponent.NumberOfPins);
-		// To Do : da qui inizia il gioco
 
-	}
+
+			for (int index = 0; index < 2; index++)
+			{
+
+				Card newCard = deck.myDeck[0];
+
+				if (index == 0)
+				{
+					newCard.transform.position = new Vector3(cockpitPosition.transform.position.x, cockpitPosition.transform.position.y, cockpitPosition.transform.position.z -zOffset);
+					newCard.transform.rotation = Quaternion.identity;
+					firstCockpit.Add(newCard);
+					deck.myDeck.RemoveAt(0);
+				}
+				else
+				{
+					newCard.transform.position = new Vector3(cockpitPosition.transform.position.x, cockpitPosition.transform.position.y, cockpitPosition.transform.position.z - zOffset);
+					newCard.transform.rotation = Quaternion.Euler(0, 0, 90);
+					secondCockpit.Add(newCard);
+					deck.myDeck.RemoveAt(0);
+				}
+
+
+			}
+			zOffset += 0.02f;
+		}
+
+			}
+
+
+	//versione in cui clonavo le carte invece di spostarle dal deck
+	//IEnumerator CreateMyStartHand()
+	//{
+		
+	//	int newIndex;
+	//	float xOffset = 0;
+	//	float zOffset = 0.03f;
+	//	float yOffset = 0;
+	//	for (int i = 0; i < 11; i++)  
+	//	{
+			
+		
+	//		for (int indexPlayer = 0; indexPlayer < 4; indexPlayer++)
+	//		{
+	//			newIndex = (indexPlayer + GetOffset()) % 4;
+				
+	//			yield return new WaitForSeconds(0.7f);
+	//			Card newCard;
+	//			if(hands[newIndex].tag == "me")
+	//			{ 
+	//				newCard = Instantiate(deck.myDeck[0], new Vector3(hands[newIndex].transform.position.x + xOffset, hands[newIndex].transform.position.y, hands[newIndex].transform.position.z - zOffset),Quaternion.identity, hands[newIndex].transform);
+	//				me.myHand.Add(newCard);
+	//				deck.myDeck.RemoveAt(0);
+					
+	//			}
+	//			else if(hands[newIndex].tag == "myMate")
+	//			{
+	//				newCard = Instantiate(deck.myDeck[0], new Vector3(hands[newIndex].transform.position.x + xOffset, hands[newIndex].transform.position.y, hands[newIndex].transform.position.z - zOffset), Quaternion.identity, hands[newIndex].transform);
+	//				myMate.myHand.Add(newCard);
+	//				deck.myDeck.RemoveAt(0);
+	//			}
+	//			else if(hands[newIndex].tag == "leftOpponent")
+	//			{
+	//				newCard = Instantiate(deck.myDeck[0], new Vector3(hands[newIndex].transform.position.x, hands[newIndex].transform.position.y - yOffset, hands[newIndex].transform.position.z - zOffset), Quaternion.Euler(0, 0, 90), hands[newIndex].transform);
+	//				leftOpponent.myHand.Add(newCard);
+	//				deck.myDeck.RemoveAt(0);
+	//			}
+	//			else
+	//			{
+	//				newCard = Instantiate(deck.myDeck[0], new Vector3(hands[newIndex].transform.position.x, hands[newIndex].transform.position.y - yOffset, hands[newIndex].transform.position.z - zOffset), Quaternion.Euler(0, 0, 90), hands[newIndex].transform);
+	//				rightOpponent.myHand.Add(newCard);
+	//				deck.myDeck.RemoveAt(0);
+	//			}
+
+	//			//newCard.IsVisible = hands[newIndex].tag == "me" ? true : false;				//vedo solo le mie
+	//			//per test le mettop tutte a true
+	//			newCard.IsVisible = true;
+	//		}
+	//		xOffset += 0.9f;
+	//		zOffset += 0.03f;
+	//		yOffset += 0.7f;
+	//	}
+
+	//	print("La lista delle mie carte è : ");
+	//	foreach(Card card in me.myHand)
+	//	{
+	//		print(card.Value + " " + card.Suit + " " + card.Color);
+	//	}
+	//	me.CountJolly();
+	//	me.CountPins();
+	//	myMate.CountJolly();
+	//	myMate.CountPins();
+	//	leftOpponent.CountJolly();
+	//	leftOpponent.CountPins();
+	//	rightOpponent.CountJolly();
+	//	rightOpponent.CountPins();
+	//	print("Il numero di Jolly che ho io è : " + me.NumberOfJolly + " Il numero di Pinelle è : " + me.NumberOfPins);
+	//	print("Il numero di Jolly che ha Left è : " + leftOpponent.NumberOfJolly + " Il numero di Pinelle  è : " + leftOpponent.NumberOfPins);
+	//	print("Il numero di Jolly che ha myMate : " + myMate.NumberOfJolly + " Il numero di Pinelle è : " + myMate.NumberOfPins);
+	//	print("Il numero di Jolly che ha Right è : " + rightOpponent.NumberOfJolly + " Il numero di Pinelle  è : " + rightOpponent.NumberOfPins);
+	//	int numOfPins = 0, numOfJolly = 0, numOfRemainingCards = 0;
+	//	foreach(Card card in deck.myDeck)
+	//	{
+	//		numOfRemainingCards++;
+	//		if (card.CanBePin)
+	//		{
+	//			numOfPins++;
+	//		}
+	//		if (card.CanBeJolly && !card.CanBePin)
+	//		{
+	//			numOfJolly++;
+	//		}
+
+	//	}
+	//	print("il numero delle carte rimasto è : " + numOfRemainingCards);
+	//	print("Il numero di Jolly rimasto è : " + numOfJolly + " Il numero di Pinelle rimaste  è : " + numOfPins);
+	//	// To Do : da qui inizia il gioco
+
+	//}
 
 	int GetOffset()
 	{
